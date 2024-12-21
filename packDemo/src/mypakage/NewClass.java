821589fef085eb74f4ab629a07d4fd212ab08e6b
Nikcode13 <100784138+Nikcode13@users.noreply.github.com>
2024-12-21 11:08:32 +0530

    Add files via upload

---
+++ packDemo/src/mypakage/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypakage;
+
+/**
+ *
+ * @author IMRD
+ */
+public class NewClass {
+    public void show()
+    {
+        System.out.println("pakage show method is called");
+    }
+}
